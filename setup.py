818b3242b8cde1738386cc3a1d808538a495abf5
Anis Ehsani <anis@mosaicml.com>
2022-06-01 14:10:12 -0700

    testbook change

--- setup.py
+++ setup.py
@@ -108,12 +108,10 @@ extra_deps["dev"] = [
     "furo==2022.3.4",
     "sphinx-copybutton==0.5.0",
     "tabulate==0.8.9",  # for auto-generating tables
-    "testbook==0.4.2",
     "myst-parser==0.16.1",
     "sphinx_panels==0.6.0",
     "sphinxcontrib-images==0.9.4",
     "pytest_codeblocks==0.15.0",
-    "traitlets==5.1.1",  # required by testbook. Version 5.2.2 has an import bug, so pinning to 5.1.1, which worked previously.
 ]
 
 extra_deps["deepspeed"] = [
